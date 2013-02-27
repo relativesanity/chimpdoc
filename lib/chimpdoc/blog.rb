ad4de901c3ebee259ff73a51cac901995c999d2f
Jonathan Barrett <j.e.barrett@gmail.com>
2013-02-27 14:00:09 +0000

    adds TomDoc to all public methods on Blog

--- lib/chimpdoc/blog.rb
+++ lib/chimpdoc/blog.rb
@@ -2,26 +2,49 @@ class Chimpdoc::Blog
 
   CACHE_KEY = 'articles'
 
+  # Public: Retrieves the most recently published article in the blog
+  #
+  # Returns an Article
   def cover_article
     feed.first
   end
 
+  # Public: Returns the publication date of the next article
+  #
+  # Intended to be used to provide information to blog readers on when the next article is
+  # predicted to hit
+  #
+  # Returns a Date
   def next_article_date
     next_article.try(:published_on)
   end
 
+  # Public: Retrieves the soonest future-published article
+  #
+  # Exists primarily for use by the next_article_date method. Should probably be private
+  #
+  # Returns an Article
   def next_article
     preview_feed.first
   end
 
+  # Public: Retrieves all currently-published articles, ordered by latest-first
+  #
+  # Returns an Enumerable collection of Articles
   def feed
     published_articles.sort_by(&:published_on).reverse
   end
 
+  # Public: Retrieves all upcoming-published articles, ordered by soonest-first
+  #
+  # Returns an Enumerable collection of Articles
   def preview_feed
     pending_articles.sort_by(&:published_on)
   end
 
+  # Public: Retrieves a specific published article based on its slug
+  #
+  # Returns an Article
   def fetch_article(slug)
     published_articles.find { |a| slug == a.slug }
   end
